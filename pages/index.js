e502a5ff1e07c21a1944ac7fae68cdb0df25d330
Rafael Gutierrez <rgutierreztejada@gmail.com>
2022-11-08 15:44:06 -0600

    adjustments

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import Layout from '@components/Layout/Layout'
 import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
 import ProductList from '@components/ProductList/ProductList'
-import Link from 'next/Link';
 
 const Home = () => {
 
